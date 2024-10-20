9b22af2f889951f5e79430f4274730f8676958df
immone <antti.imm1n@gmail.com>
2024-10-20 15:53:02 +0300

    proposed new content

--- 09-exercise/ECC_ex_assignment/elgamal.py
+++ 09-exercise/ECC_ex_assignment/elgamal.py
@@ -2,43 +2,189 @@ from ecc import EC
 
 class ElGamal(object):
     """ElGamal Encryption
-    public key encryption as replacing (mulmod, powmod) to (ec.add, ec.mul)
+    Public key encryption as replacing (mulmod, powmod) to (ec.add, ec.mul)
     - ec: elliptic curve
     - g: (random) a point on ec
     """
+    
     def __init__(self, ec, g, n):
+        """Initialize the ElGamal encryption with the elliptic curve, 
+        base point g, and the order n of the curve.
+        
+        Parameters:
+        - ec: an instance of EC representing the elliptic curve
+        - g: a point on the elliptic curve
+        - n: order of the elliptic curve
+        
+        >>> elgamal = ElGamal(ec_instance, g_point, n_order)
+        >>> assert elgamal.ec == ec_instance
+        >>> assert elgamal.g == g_point
+        >>> assert elgamal.n == n_order
+        """
         assert isinstance(ec, EC)
         assert ec.is_valid(g)
         self.ec = ec
         self.g = g
         self.n = n
-        pass
 
     def gen(self, sk):
-        """generate public key
+        """Generate public key.
+        
+        Parameters:
         - sk: private key as (random) int < ec.q
-        - returns: public key as a point on ec
+        
+        Returns:
+        - Randomized public key as a point on ec
+        
+        TODO:
+        Exercise 1:
+        
+        Implement the missing method logic to properly randomize public key generation.
+        The randomization should produce an integer that is large enough; for this, utilize 
+        a random integer of at least 20 bits.
+        
+        This method should ensure that the key is securely randomized according to the
+        lecture notes.
         """
         return self.ec.mul(self.g, sk)
 
     def enc(self, plaintext, pk, r):
-        """encrypt
+        """Encrypt the plaintext using the public key and a random integer.
+        
+        Parameters:
         - plaintext: data as a point on ec
         - pk: public key as a point on ec
         - r: random int < ec.q
-        - returns: (ciphertext1, cipertext2) as points on ec
+        
+        Returns:
+        - (ciphertext1, ciphertext2) as points on ec
         """
         assert self.ec.is_valid(plaintext)
         assert self.ec.is_valid(pk)
         return (self.ec.mul(self.g, r), self.ec.add(plaintext, self.ec.mul(pk, r)))
 
     def dec(self, ciphertext, sk):
-        """decrypt
-        - ciphertext: (ciphertext1, cipertext2) as points on ec
+        """Decrypt the ciphertext using the private key.
+        
+        Parameters:
+        - ciphertext: (ciphertext1, ciphertext2) as points on ec
         - sk: private key as int < ec.q
-        - returns: plaintext as a point on ec
+        
+        Returns:
+        - plaintext as a point on ec
         """
         c1, c2 = ciphertext
         assert self.ec.is_valid(c1) and self.ec.is_valid(c2)
         return self.ec.add(c2, self.ec.neg(self.ec.mul(c1, sk)))
-    pass
+
+    def randomize_key(self, sk):
+        """Randomizes secret key.
+        
+        Parameters:
+        - sk: a secret key
+        
+        Returns:
+        - A randomized key
+        
+        TODO:
+        Exercise 1:
+        
+        Implement the method logic to properly randomize a secret key.
+        
+        This method should ensure that the key is securely randomized according to the
+        lecture notes.
+        """
+        pass
+
+    def randomize_key(self, sk):
+        """Randomizes secret key.
+        
+        Parameters:
+        - sk: a secret key
+        
+        Returns:
+        - A randomized key
+        
+        TODO:
+        Exercise 1:
+        
+        Implement the method logic to properly randomize a secret key.
+        
+        This method should ensure that the key is securely randomized according to the
+        lecture notes.
+        """
+        pass
+
+    def random_ec_points(self):
+        """Generates random elliptic curve points for point blinding countermeasure.
+
+        Returns:
+        - R: A point on the elliptic curve, modified based on a random choice.
+        - S: Another point on the elliptic curve, modified similarly.
+
+        TODO:
+        Exercise 2:
+        
+        Implement the logic to randomize the generated elliptic curve points R and S. 
+        This can be implemented by involve multiplying or negating the points based randomly
+        on a coin flip, so that R and S remain on the elliptic curve.
+        """
+        # Hardcoded points for point blinding countermeasure:
+        
+        R = [
+            42379038535447972425882922920705906187614560245489370485065143754481248749916, 
+            7157235819141609872407131958606778042988197471338048906967563567681524627747
+        ]
+        S = [
+            12023092955571775742129462795245908043695328604996865184404062632842725671078, 
+            69719650118977673464433968526791304600352811755443486686633735756640764713805
+        ]
+        
+        return R, S
+
+
+    def enc_point_blind(self, R, plaintext, pk, r):
+        """Encrypts a plaintext point using point blinding.
+
+        Parameters:
+        - R: A point on the elliptic curve used for blinding.
+        - plaintext: data as a point on ec
+        - pk: public key as a point on ec
+        - r: random int < ec.q
+
+        Returns:
+        - A tuple containing (ciphertext1, ciphertext2) as points on the elliptic curve.
+
+        TODO:
+        Exercise 2:
+
+        Implement the method logic to properly encrypt a point on the elliptic curve 
+        using point blinding. Ensure that the function validates the inputs and 
+        applies the blinding to the first ciphertext before returning the results.
+        """
+        # Validate that the plaintext and public key are valid elliptic curve points
+        assert self.ec.is_valid(plaintext)
+        assert self.ec.is_valid(pk)
+
+        pass 
+
+
+    def dec_point_blind(self, S, ciphertext, sk):
+        """Decrypts a blinded ciphertext using the secret key.
+
+        Parameters:
+        - S: A point on the elliptic curve used for blinding.
+        - ciphertext: Tuple containing the blinded ciphertext (ciphertext1, ciphertext2).
+        - sk: private key as int < ec.q
+
+        Returns:
+        - The original plaintext point on the elliptic curve.
+
+        TODO:
+        Exercise 2:
+
+        Implement the decryption logic to retrieve the original plaintext from 
+        the blinded ciphertext. 
+        """
+
+        pass
--- 09-exercise/ECC_ex_assignment/main_ecc.py
+++ 09-exercise/ECC_ex_assignment/main_ecc.py
@@ -3,49 +3,73 @@ from elgamal import ElGamal
 import random
 
 if __name__ == "__main__":
-    # shared elliptic curve system of examples
-    # parameters for the curve P-256
-    ec = EC(0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc,
-            0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b,
-            0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff)
-    
-    g = (0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296, 0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5)
+    # NOTE FOR STUDENTS:
+    # Do NOT modify any of the variable values in this code.
+    # These values are required for unit tests to pass correctly.
+
+    # Parameters for the elliptic curve P-256
+    curve_a = 0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc
+    curve_b = 0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b
+    curve_p = 0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff
+
+    # Initialize the elliptic curve
+    ec = EC(curve_a, curve_b, curve_p)
+
+    # Define generator point (g) on the elliptic curve
+    g = (
+        0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296, 
+        0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5
+    )
+
+    # Order of the group 
     n = 0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551
-    print("Order:", n)
-    print("Generator:", ec.q)
+    
+    # Verify the order of the group
+    print("Order of the group:", n)
+    print("Field size of the curve (generator):", ec.q)
     print("")
     assert n <= ec.q
- 
-    # ElGamal enc/dec usage
-    eg = ElGamal(ec, g, n)              #set the EC, generator and order for the encryption scheme
 
-    plaintext = ec.mul(g,random.randint(1,n-1))
+    # Initialize ElGamal encryption scheme with elliptic curve, generator point, and n
+    eg = ElGamal(ec, g, n)
+
+    # Generate a random plaintext point on the elliptic curve
+    random_scalar = random.randint(1, n - 1)
+    plaintext = ec.mul(g, random_scalar)
+    
+    # Ensure the plaintext is a valid point on the curve
     assert ec.is_valid(plaintext)
-    print("Plaintext:", plaintext)
+    print("Generated plaintext:", plaintext)
     print("")
 
-    #Hard-coded secret key
-    sk = 94952102889125874165031048266763684604430453914299026099439664202419944786514
-    print("Secret key:", sk)
+    # Hard-coded secret key
+    secret_key = 94952102889125874165031048266763684604430453914299026099439664202419944786514
+    print("Secret key:", secret_key)
     print("")
 
-    #Key generation
-    pk = eg.gen(sk)
-    assert ec.is_valid(pk)
-    print("Public key:", pk)
+    # Key generation (public key)
+    public_key = eg.gen(secret_key)
+
+    # Ensure the public key is valid
+    assert ec.is_valid(public_key)
+    print("Generated public key:", public_key)
     print("")
 
-    #Encryption
-    ciphertext = eg.enc(plaintext, pk, random.randint(1,n-1))
-
-    #Decryption
-    decoded = eg.dec(ciphertext, sk)
+    # Encryption of the plaintext using the public key
+    random_scalar_2 = random.randint(1, n - 1)
+    ciphertext = eg.enc(plaintext, public_key, random_scalar_2)
+    
+    # Decryption using the secret key
+    recovered_plaintext = eg.dec(ciphertext, secret_key)
 
     print("Ciphertext:", ciphertext)
     print("")
-    print("Recovered plaintext:", decoded)
-    assert decoded == plaintext
-    assert ciphertext != pk
+    print("Recovered plaintext:", recovered_plaintext)
 
+    # Ensure the decryption recovered the original plaintext
+    assert recovered_plaintext == plaintext
+    assert ciphertext != public_key
 
-    
+    ##
+
+    # Feel free to test your implementations for Exercise 1 and Exercise 2 here in a similar fashion!
